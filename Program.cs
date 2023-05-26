22722d382ddd0ef1e51de43cbf3e6ab81e3d0703
Mert Ernaz <rnzappdroid@gmail.com>
2023-05-27 00:05:29 +0300

    Replace char

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structures
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Cümle giriniz : ");
+            string s=Console.ReadLine();
+           
+            string[] kelimeler=s.Split(" ");
+            
+
+            ReplaceFirstLast(kelimeler);
+        }
+
+        public static void ReplaceFirstLast(string[] words){
+            foreach(var x in words){
+                char xFirst,xLast;
+                xFirst=x[0];
+                xLast=x[x.Length-1];
+                int counter=0;
+
+                foreach(var y in x){
+                    if(counter==0||counter==x.Length-1){
+                        if(x.Length!=1){
+                            if(counter==0){Console.Write(xLast);}
+                            if(counter==x.Length-1){Console.Write(xFirst);}
+                        }else{
+                             Console.Write(y);
+                        }
+                        
+                    }else{
+                        Console.Write(y);
+                    }
+                    
+                    counter++;
+                }
+                Console.Write(" ");                
+
+            }
+        }
+    }
+}
